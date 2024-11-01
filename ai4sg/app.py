90418a794bfe8a21ba2a2e1f1aa5fb06a9cba42f
YM <youssefmasoud4@gmail.com>
2024-11-01 01:37:16 -0400

    latest update

--- ai4sg/app.py
+++ ai4sg/app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    #return render_template('home.html')
+    return '<h1>Hello!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
